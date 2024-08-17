badc90851b70d224345360162d9caca9a85a4178
GameTester228 <ymkaf@mail.ru>
2024-08-17 18:31:58 +0300

    0.0.1.1

--- src/main (2).jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css"; // Подключаем стили Bootstrap
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-
-const root = document.getElementById("root");
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"; // Подключаем стили Bootstrap
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+const root = document.getElementById("root");
 
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
